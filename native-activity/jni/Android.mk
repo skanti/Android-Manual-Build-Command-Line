39d238bfbf1052c0e106ad6f141c90fc2164a22b
Armen Avetisyan <armen.avetisyan.to@gmail.com>
2016-08-30 12:26:35 +0900

    Implementing ndk-build.

---
+++ native-activity/jni/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := native-activity
+LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
